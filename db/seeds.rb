8d74655234bc103361543a60386f8fa6221011a1
Nahid1911 <nahid.raihan@gmail.com>
2023-09-27 17:00:38 +0600

    Add api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+greeting_msgs = ['Hello React', 'Hello Rails', 'Hello React-Rails', 'Hello Microverse', 'Happy coding']
+
+greeting_msgs.each do |msg|
+    Message.create(content: msg)
+end
