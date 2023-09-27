45e6987fd7559611d34448fd798b4c5e0c35a1ff
Nahid1911 <nahid.raihan@gmail.com>
2023-09-27 17:21:53 +0600

    Add linters to the project

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,6 +1,10 @@
-class Api::GreetingsController < ApplicationController
-  def random
-    random_greeting = Message.order('RANDOM()').first
-    render json: { message: random_greeting.content }
+# frozen_string_literal: true
+
+module Api
+  class GreetingsController < ApplicationController
+    def random
+      random_greeting = Message.order('RANDOM()').first
+      render json: { message: random_greeting.content }
+    end
   end
 end
